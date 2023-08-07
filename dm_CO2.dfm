420dd6c67417ad730361e2aa6ee13d9126c7f895
James Easson <jameseasson05@gmail.com>
2023-08-07 12:23:44 +0200

    Back up

---
+++ dm_CO2.dfm
@@ -0,0 +1,48 @@
+object DMCO2: TDMCO2
+  OldCreateOrder = False
+  Height = 221
+  Width = 326
+  object ConCO2: TADOConnection
+    Connected = True
+    ConnectionString = 
+      'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\13236\Down' +
+      'loads\Grade12-pat-main\Grade12-pat-main\Grade 12 PAT phase 2\Dat' +
+      'abase\db_CO2.mdb;Mode=Share Deny None;Persist Security Info=Fals' +
+      'e;Jet OLEDB:System database="";Jet OLEDB:Registry Path="";Jet OL' +
+      'EDB:Database Password="";Jet OLEDB:Engine Type=5;Jet OLEDB:Datab' +
+      'ase Locking Mode=1;Jet OLEDB:Global Partial Bulk Ops=2;Jet OLEDB' +
+      ':Global Bulk Transactions=1;Jet OLEDB:New Database Password="";J' +
+      'et OLEDB:Create System Database=False;Jet OLEDB:Encrypt Database' +
+      '=False;Jet OLEDB:Don'#39't Copy Locale on Compact=False;Jet OLEDB:Co' +
+      'mpact Without Replica Repair=False;Jet OLEDB:SFP=False'
+    LoginPrompt = False
+    Mode = cmShareDenyNone
+    Provider = 'Microsoft.Jet.OLEDB.4.0'
+    Left = 24
+    Top = 72
+  end
+  object ADOUsers: TADOTable
+    Active = True
+    Connection = ConCO2
+    CursorType = ctStatic
+    TableName = 'tblUsers'
+    Left = 88
+    Top = 24
+  end
+  object ADOCars: TADOTable
+    Active = True
+    Connection = ConCO2
+    CursorType = ctStatic
+    TableName = 'tblCar'
+    Left = 88
+    Top = 88
+  end
+  object ADOOrganisation: TADOTable
+    Active = True
+    Connection = ConCO2
+    CursorType = ctStatic
+    TableName = 'tblOrganisation'
+    Left = 88
+    Top = 144
+  end
+end
---
+++ u_SignIn.pas
@@ -0,0 +1,90 @@
+unit u_SignIn;
+
+interface
+
+uses
+  u_passwordhasher, dm_CO2, Dialogs, Forms,sysUtils;
+
+type
+  SignIn = class(tObject)
+  private
+    fUsername: String;
+    fPassword: string;
+    fAttempts: integer;
+  public
+    constructor create;
+    procedure SetLogin(sUsername, sPassword: string);
+    Function PasswordCheck: boolean;
+  end;
+  Signup= class(tObject)
+    private
+    fFirst_name:string;
+    fLast_name:string;
+    fUsername:string;
+    fPassword:string;
+    public
+    constructor create(sFirst_name,sLast_name:string);
+    function UsernameGeneration:string;
+  end;
+
+implementation
+
+{ SignIn }
+
+constructor SignIn.create();
+begin
+  fUsername := '';
+  fPassword := '';
+  fAttempts := 0;
+end;
+
+function SignIn.PasswordCheck: boolean;
+begin
+  if fAttempts <= 3 then
+  begin
+    with dmCO2 do
+    begin
+      ADOUsers.first;
+      while not ADOUsers.eof do
+      begin
+        if (fUsername = ADOUsers['Username']) and
+          (hash(fPassword) = ADOUsers['Password']) then
+          result := true
+        else
+          ADOUsers.Next;
+      end;
+      result := false;
+      inc(fAttempts);
+      MessageDlg('Please check your username and password.', mtWarning, [mbOk],
+        0);
+      exit;
+    end;
+
+  end
+  else
+    MessageDlg('Maxmium number of attempts reached. Terminating program.',
+      mtError, [mbOk], 0);
+  application.Terminate;
+
+end;
+
+procedure SignIn.SetLogin(sUsername, sPassword: string);
+begin
+  fUsername := sUsername;
+  fPassword := sPassword;
+end;
+
+{ Signup }
+
+constructor Signup.create(sFirst_name, sLast_name: string);
+begin
+fFirst_name:=sFirst_name;
+fLast_name:=sLast_name;
+end;
+
+function Signup.UsernameGeneration: string;
+begin
+fUsername:=fFirst_name+fLast_name+inttostr(random(100));
+end;
+
+end.
